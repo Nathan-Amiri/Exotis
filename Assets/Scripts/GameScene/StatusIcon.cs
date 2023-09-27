ca3b9d4c057c1c7fb2bd45f04d65e5efbfc4849b
azeTrom17 <nmort17@aol.com>
2023-09-27 02:51:38 -0500

    added status icons to layout

---
+++ Assets/Scripts/GameScene/StatusIcon.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusIcon : MonoBehaviour
+{
+    //assigned in prefab
+    [SerializeField] private Image iconImage;
+    [SerializeField] private TMP_Text initialsText;
+
+
+}
